4995874858dd73af5e485d960647b410aad6af42
Oleg <sod12@yandex.ru>
2021-09-09 20:33:10 +0500

    2

--- task_16.py
+++ task_16.py
@@ -4,5 +4,30 @@ with open('db.csv', 'r', encoding='utf-8') as fin:
     data_db = csv.reader(fin, delimiter = ";")
     for row in data_db:
         listDB.append(tuple(row))
+DB = sqlite3.connect('data.bd')
 
-print(listDB)
+link = DB.cursor()
+# link.execute('''CREATE TABLE IF NOT EXISTS human(
+#    'Корпорация',
+#    'Филиал',
+#    'Бизнес-центр',
+#    'Адрес бизнес-центра',
+#    'Должность сотрудника',
+#    'ФИО сотрудника',
+#    'Телефон сотрудника', 
+#    'Отдел', 
+#    'Статус руководителя', 
+#    'Документы', 
+#    'Подпись сотрудника', 
+#    'Подпись руководителя', 
+#    'Реквизит отдела', 
+#    'Реквизит бизнес-центра', 
+#    'Реквизит филиала', 
+#    'Реквизит корпорации');''')
+# DB.commit()
+
+listDB.pop(0)
+link.executemany("INSERT INTO human VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", listDB)
+DB.commit()
+t = link.execute('''SELECT * FROM human''').fetchall()
+DB.close()
