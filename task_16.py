15717f9a52e6aa8c4b2447d7eb3e0094de1f54dd
Oleg <sod12@yandex.ru>
2021-09-09 19:17:57 +0500

    first commit

---
+++ task_16.py
@@ -0,0 +1,8 @@
+import csv
+listDB = []
+with open('db.csv', 'r', encoding='utf-8') as fin:
+    data_db = csv.reader(fin, delimiter = ";")
+    for row in data_db:
+        listDB.append(tuple(row))
+
+print(listDB)
